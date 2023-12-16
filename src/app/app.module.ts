e6e3a6edabc7358c3f1e885f07125aac0657e247
Miguel Bulla Duque <miggaeton0926@gmail.com>
2023-12-15 20:48:04 -0500

    readme updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
 import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
